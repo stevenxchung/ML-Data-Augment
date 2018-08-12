b1a52da8e9da8fc5c31bc64e885742d7135520f6
Steven <stevenxchung@gmail.com>
2018-08-12 12:21:32 -0400

    Add transformations

--- operations.py
+++ operations.py
@@ -19,3 +19,22 @@ def random_noise(image_array: ndarray):
 def horizontal_flip(image_array: ndarray):
     # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
     return image_array[:, ::-1]
+
+# dictionary of the transformations functions we defined earlier
+available_transformations = {
+    'rotate': random_rotation,
+    'noise': random_noise,
+    'horizontal_flip': horizontal_flip
+}
+
+# random num of transformations to apply
+num_transformations_to_apply = random.randint(
+    1, len(available_transformations))
+
+num_transformations = 0
+transformed_image = None
+while num_transformations <= num_transformations_to_apply:
+    # choose a random transformation to apply for a single image
+    key = random.choice(list(available_transformations))
+    transformed_image = available_transformations[key](image_to_transform)
+    num_transformations += 1
